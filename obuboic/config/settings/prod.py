997a418df1af0c52e4970a68c32ce4cc6607ab05
jetty <dlwnsdud980727@gmail.com>
2024-03-27 09:37:58 +0900

    feat: 로그 설정
    - 로그 파일 추가
    - CORS 허용 대상 변경

--- obuboic/config/settings/prod.py
+++ obuboic/config/settings/prod.py
@@ -5,3 +5,10 @@ STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 
 DEBUG = False
+
+CORS_ALLOWED_ORIGINS = [
+    'http://3.38.12.213',
+    'https://3.38.12.213',
+    'http://nursinghome.ai',
+    'https://nursinghome.ai',
+]
