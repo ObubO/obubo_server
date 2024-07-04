d0b0e0f6a3df2871bb4044a7051f94c5093bbc2f
jetty <dlwnsdud980727@gmail.com>
2024-07-04 15:56:35 +0900

    API url 수정

--- obuboic/accounts/urls.py
+++ obuboic/accounts/urls.py
@@ -3,24 +3,20 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('join/', views.UserCreateView.as_view()),
-    path('join/check-id/<str:username>/', views.UserCreateView.as_view()),
-    path('join/check-name/<str:kname>/', views.CheckMemberName.as_view()),
+    path('join', views.UserCreateView.as_view()),
+    path('join/check-id/<str:username>', views.UserCreateView.as_view()),
+    path('join/check-name/<str:name>', views.CheckMemberName.as_view()),
 
-    path('login/', views.LoginView.as_view()),
-    path('logout/', views.LogoutView.as_view()),
+    path('login', views.LoginView.as_view()),
+    path('logout', views.LogoutView.as_view()),
 
-    path('token/refresh/', views.CustomTokenRefreshView.as_view()),
+    path('auth/request', views.AuthRequest.as_view()),
+    path('auth/request/users', views.AuthUserRequest.as_view()),
+    path('auth/request/users/<str:username>', views.AuthUserRequest.as_view()),
+    path('auth/verify', views.AuthVerify.as_view()),
 
-    path('auth/request/', views.AuthRequest.as_view()),
-    path('auth/request/users/', views.AuthUserRequest.as_view()),
-    path('auth/request/users/<str:username>/', views.AuthUserRequest.as_view()),
-    path('auth/verify/', views.AuthVerify.as_view()),
-
-    path('user/profiles/', views.AuthView.as_view()),
-    path('user/password/', views.ChangePassword.as_view()),
-
-
+    path('users/profiles', views.AuthView.as_view()),
+    path('users/password', views.ChangePassword.as_view()),
+    path('users/token/refresh', views.CustomTokenRefreshView.as_view()),
 
 ]
-
