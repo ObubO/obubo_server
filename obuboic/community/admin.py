a8562518e0ae31dc6a7f3bdf98f195c8d566278a
jetty <dlwnsdud980727@gmail.com>
2025-04-24 23:16:43 +0900

    refactor: 커뮤니티 Model 리팩토링

--- obuboic/community/admin.py
+++ obuboic/community/admin.py
@@ -1,40 +1,64 @@
 from django.contrib import admin
 
-from .models import Post, Comment, PostLike, CommentLike
+from .models import Category, Post, Comment, PostLike, CommentLike
 
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)      # 관리자 리스트 페이지에서 보여줄 컬럼
+    search_fields = ("name",)           # 상단 검색창으로 검색할 필드
+    ordering = ("order", "id")          # 정렬 기준 (order, id 순)
+    readonly_fields = ('created_at',)
+    fieldsets = (
+        ('기본 정보', {
+            'fields': ('name', 'order')
+        }),
+        ('시스템 정보', {
+            'fields': ('is_active', 'created_at',)
+        }),
+    )
+
+
+@admin.register(Post)
 class PostsAdmin(admin.ModelAdmin):
-    fields = ['author', 'title', 'content', 'created_at', 'updated_at']
     list_display = ('id', 'title', 'author', )
-
-    readonly_fields = ['created_at', 'updated_at', ]
-
     search_fiedls = ('author', 'title')
-    ordering = ('-created_at', )
+    ordering = ('-created_at',)
+    readonly_fields = ['created_at', 'updated_at', ]
+    fieldsets = (
+        ('기본 정보', {
+            'fields': ('title', 'author', 'content')
+        }),
+        ('시스템 정보', {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
 
 
+@admin.register(Comment)
 class CommentsAdmin(admin.ModelAdmin):
-    fields = ['id', 'post', 'author', 'content', 'created_at', 'parent']
     list_display = ('id', 'content', 'post', 'author',)
-
-    readonly_fields = ['id', 'created_at', 'parent']
-
     search_fiedls = ('author', 'post')
     ordering = ('-created_at',)
+    readonly_fields = ['parent', 'created_at', 'updated_at']
+    fieldsets = (
+        ('기본 정보', {
+            'fields': ('post', 'author', 'content')
+        }),
+        ('시스템 정보', {
+            'fields': ('parent', 'created_at', 'updated_at')
+        }),
+    )
 
 
+@admin.register(PostLike)
 class PostLikeAdmin(admin.ModelAdmin):
-    fields = ['id', 'user', 'post']
+    fields = ['user', 'post']
     list_display = ['id', 'user', 'post']
 
 
+@admin.register(CommentLike)
 class CommentLikeAdmin(admin.ModelAdmin):
-    fields = ['id', 'user', 'comment']
+    fields = ['user', 'comment']
     list_display = ['id', 'user', 'comment']
-
-
-admin.site.register(Post, PostsAdmin)
-admin.site.register(Comment, CommentsAdmin)
-admin.site.register(PostLike, PostLikeAdmin)
-admin.site.register(CommentLike, CommentLikeAdmin)
--- obuboic/community/models.py
+++ obuboic/community/models.py
@@ -1,51 +1,62 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 from accounts.models import User
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=10)
-
-    objects = models.Manager()
+    name = models.CharField(verbose_name="카테고리명", max_length=20)
+    order = models.PositiveIntegerField(verbose_name="노출 순서", default=0)
+    is_active = models.BooleanField(verbose_name="사용 여부", default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
+        db_table = 'post_category'
         verbose_name = "게시판 카테고리"
+        verbose_name_plural = "게시판 카테고리 목록"
 
     def __str__(self):
-        return self.category_name
+        return self.name
 
 
 class Post(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="posts")
-    title = models.CharField(_("title"), max_length=100)
-    content = models.TextField(_("content"))
-    views = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name="posts")
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="user_posts")
     like = models.ManyToManyField(User, through="PostLike", through_fields=("post", "user"), related_name="like_posts")
+
+    title = models.CharField(verbose_name="제목", max_length=100)
+    content = models.TextField(verbose_name="내용")
+    views = models.PositiveIntegerField(verbose_name="조회수", default=0)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     objects = models.Manager()
 
     class Meta:
+        db_table = 'post'
         verbose_name = "게시글"
+        verbose_name_plural = "게시글 목록"
 
     def __str__(self):
         return self.title
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="comments")
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, related_name="comments")
-    content = models.TextField(_("content"))
-    like = models.ManyToManyField(User, through="CommentLike", through_fields=("comment", "user"), related_name="like_comments")
-    created_at = models.DateTimeField(auto_now_add=True)
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name="replies")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="user_comments")
+    like = models.ManyToManyField(User, through="CommentLike", through_fields=("comment", "user"),
+                                  related_name="like_comments")
+
+    content = models.TextField(verbose_name='내용')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     objects = models.Manager()
 
     class Meta:
+        db_table = 'comment'
         verbose_name = "댓글"
+        verbose_name_plural = "댓글 목록"
 
     def __str__(self):
         return self.content[:20]
@@ -54,21 +65,34 @@ class Comment(models.Model):
 class PostLike(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     objects = models.Manager()
 
     class Meta:
+        db_table = 'post_like'
         verbose_name = "게시글 좋아요"
+        verbose_name_plural = "게시글 좋아요 목록"
         unique_together = ('user', 'post')
+
+    def __str__(self):
+        return f"{self.user} - {self.post}"
 
 
 class CommentLike(models.Model):
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     objects = models.Manager()
 
     class Meta:
+        db_table = 'comment_like'
         verbose_name = "댓글 좋아요"
+        verbose_name_plural = "댓글 좋아요 목록"
         unique_together = ('user', 'comment')
+
+    def __str__(self):
+        return f"{self.user} - {self.comment}"
+
 
